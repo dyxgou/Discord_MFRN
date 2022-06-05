da0d19c3be10d59c647b3735344b6147d8d2fb52
iDiego5614 <diego5614yt@gmail.com>
2022-06-05 00:11:58 -0500

    feat(backend): ⚒️  Backend Plugins

---
+++ backend/src/plugins/support.ts
@@ -0,0 +1,31 @@
+import fp from 'fastify-plugin'
+import checkJWT, { IPayload, IUser } from '../services/checkJWT'
+import connectDB, { IStore } from '../services/connectDB'
+import regsiterJWT from '../services/registerJWT'
+
+export interface SupportPluginOptions {
+  // Specify Support plugin options here
+}
+
+// The use of fastify-plugin is required to be able
+// to export the decorators to the outer scope
+export default fp<SupportPluginOptions>(async (fastify, opts) => {
+  void fastify.register(connectDB)
+  void fastify.register(checkJWT)
+  void fastify.register(regsiterJWT)
+})
+
+// When using .decorate you have to specify added properties for Typescript
+declare module 'fastify' {
+  export interface FastifyInstance {
+    store : IStore
+  }
+}
+
+declare module '@fastify/jwt' {
+  export interface FastifyJWT
+  {
+    payload : IPayload,
+    user : IUser
+  }
+}
